7c2caeffe1577090e682dd9ee2ee700e682d4f93
ceestrong17 <ceestrong17@gmail.com>
2026-08-11 22:53:53 -0500

    Initial commit: Spring Boot scaffold with ping endpoint

---
+++ src/main/java/com/ceestrong/steaminsights/SteamInsightsApplication.java
@@ -0,0 +1,13 @@
+package com.ceestrong.steaminsights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamInsightsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteamInsightsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ceestrong/steaminsights/SteamInsightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceestrong.steaminsights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamInsightsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
